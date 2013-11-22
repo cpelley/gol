0ebcc98a1f5dbd41b6ae688f14be95191a8e8a28
Carwyn Pelley <cpelley.pub@gmail.com>
2013-11-22 22:29:48 +0000

    curses view

--- runme.py
+++ runme.py
@@ -1,6 +1,8 @@
+import curses
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.lib.stride_tricks as stride
+import time
 
 
 def window_views(grid, xsize=3, ysize=3, xstep=1, ystep=1):
@@ -43,7 +45,21 @@ def plot(grid):
 
 
 def main():
-    grid = np.random.randint(0, 2, (102, 102))
+
+    # Make the cursor visible
+    stdscr = curses.initscr()
+    # No echo of input characters
+    curses.noecho()
+    # Visibility of cursor
+    curses.curs_set(1)
+    stdscr.refresh()
+
+
+
+
+    size = (37, 72)
+    grid = np.random.randint(0, 2, size)
+    grid.astype(np.uint8)
     grid[0, :] = grid[-1, :] = grid[:, 0] = grid[:, -1] = 0
 
     views = window_views(grid)
@@ -52,11 +68,12 @@ def main():
                      [True, False, True],
                      [True, True, True]])
 
-    for i in xrange(1000):
-        print i
-        plot(grid)
-        for view in views:
-            for view_sub in view:
+    for i in xrange(10000):
+#        print i
+        #plot(grid)
+#        break
+        for ind, view in enumerate(views):
+            for inds, view_sub in enumerate(view):
                 neighbours = np.sum(view_sub[mask] > 0)
                 # Cell is alive.
                 if view_sub[1, 1]:
@@ -66,9 +83,18 @@ def main():
                         view_sub[1, 1] = 0
                 # Cell is dead.
                 else:
-                    if neighbours is 3:
-                        print 'reproduce'
+                    if neighbours == 3:
                         view_sub[1, 1] += 1
+
+                stdscr.addch(ind + 1, inds + 1, str(neighbours))
+                stdscr.addch(ind + 1, inds + 1 + 73, '1' if view_sub[1, 1] else '0')
+        stdscr.addstr(40, 5, str(i))
+        stdscr.refresh()
+
+    curses.nocbreak()
+    stdscr.keypad(0)
+    curses.echo()
+    curses.endwin()
 
 
 if __name__ == '__main__':
