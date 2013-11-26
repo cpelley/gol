2e9a3016b36be8f361af3c9ed480234dc1f225c4
Carwyn Pelley <cpelley.pub@gmail.com>
2013-11-26 08:08:32 +0000

    DOC: Updated doctrings.

--- runme.py
+++ runme.py
@@ -9,6 +9,11 @@ def window_views(grid, xsize=3, ysize=3, xstep=1, ystep=1):
     """
     Generate view ndarray of the grid.
 
+    Args:
+
+    * grid (2darray)
+        Grid on which to stride.
+
     Kwargs:
 
     * xsize (int):
@@ -21,11 +26,8 @@ def window_views(grid, xsize=3, ysize=3, xstep=1, ystep=1):
         Window dim-1 step, corresponding to window center.
 
     Returns:
-        Numpy ndarray which represents a view of the grid.
 
-    .. note::
-
-    This function is not currently in use.
+        Numpy ndarray which represents a views of the grid.
 
     """
     strides = (grid.strides[0] * xstep,
@@ -39,13 +41,59 @@ def window_views(grid, xsize=3, ysize=3, xstep=1, ystep=1):
 
 
 def mpl_plot(grid):
-    plt.figure()
+    """
+    Simple predefined plotting using matplotlib.
+
+    Args:
+
+    * grid (2darray):
+        Grid to plot.
+
+    Returns:
+
+        None
+
+    """
+    plt.gcf()
     plt.pcolor(grid)
     plt.show()
 
 
-class CursePlot(object):
+class Plot(object):
+    """Abstract class for a GOL simulation plotting backend."""
+    def __enter__(self):
+        raise NotImplementedError()
+
+    def update(self):
+        raise NotImplementedError()
+
+    def __exit__(self):
+        raise NotImplementedError()
+
+
+class CursePlot(Plot):
+    """Curses context manager for fast plotting an ndarray."""
     def __init__(self, grid, window=None, fps=None):
+        """
+        Curses context manager for fast plotting an ndarray.
+
+        Args:
+
+        * grid (2darray):
+            Grid to plot.
+
+        Kwargs:
+
+        * window (ndarray):
+            ndarray view of the grid (see :func:`window_views`).
+        * fps (int):
+            ~ Numbers of frames per second.
+
+        Returns:
+
+            None
+
+        """
         self.fps = fps
         if window:
             self.window = window
@@ -55,9 +103,9 @@ class CursePlot(object):
 
         # No echo of input characters
         curses.noecho()
-        curses.start_color()
-        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_BLACK)
-        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_WHITE)
+        #curses.start_color()
+        #curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_BLACK)
+        #curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_WHITE)
 
         # Visibility of cursor
         curses.curs_set(0)
@@ -66,6 +114,10 @@ class CursePlot(object):
         return self
 
     def update(self):
+        """
+        Update the curses window with the current content of our grid.
+
+        """
         for ind, line in enumerate(self.grid):
             pout = ''.join(line.astype('|S1')).replace('0', ' ')
             self.window.addstr(ind, 0, pout)
@@ -75,13 +127,31 @@ class CursePlot(object):
             time.sleep(1. / self.fps)
 
     def __exit__(self):
+        # Close curses.
         curses.nocbreak()
         self.stdscr.keypad(0)
         curses.echo()
         curses.endwin()
 
 
-def game_of_life(size=(40, 140), plot_tool=CursePlot):
+def game_of_life(size=(40, 140), plot_tool=CursePlot, iterations=1000):
+    """
+    Game of life simulation.
+
+    Kwargs:
+
+    * size (length 2 tuple):
+        Grid size dimensions, expected in (size_y, size_x).
+    * plot_tool (:class:`Plot` subclass):
+        Plotting backend for plotting each GOL iteration.
+    * iterations (int):
+        Number of iterations for the simulation.
+
+    Returns:
+
+        None
+
+    """
     # Adjust dimensions to suitable value
     size = [dim + (2 - diff) for dim, diff in
             zip(size, [ss % 3 for ss in size])]
@@ -97,7 +167,7 @@ def game_of_life(size=(40, 140), plot_tool=CursePlot):
                      [True, True, True]])
 
     with plot_tool(grid) as plot:
-        for i in xrange(1000):
+        for i in xrange(iterations):
             neighbours = views[..., mask].sum(2)
             views[..., 1, 1] = (views[..., 1, 1] & ~(neighbours > 3) &
                                 ~(neighbours < 2) | neighbours == 3)
